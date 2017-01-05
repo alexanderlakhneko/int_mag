ffe9876016f2f35d2f36e235aa09fa2558de3802
alexanderlakhneko <alexanderlakhneko@gmail.com>
2017-01-05 13:06:27 +0200

    Default layout

--- src/View/default_layout.php
+++ src/View/default_layout.php
@@ -6,23 +6,23 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Главная</title>
-    <link href="/template/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/template/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/template/css/prettyPhoto.css" rel="stylesheet">
-    <link href="/template/css/price-range.css" rel="stylesheet">
-    <link href="/template/css/animate.css" rel="stylesheet">
-    <link href="/template/css/main.css" rel="stylesheet">
-    <link href="/template/css/responsive.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/prettyPhoto.css" rel="stylesheet">
+    <link href="/css/price-range.css" rel="stylesheet">
+    <link href="/css/animate.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/responsive.css" rel="stylesheet">
 
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
+    <script src="/js/html5shiv.js"></script>
+    <script src="/js/respond.min.js"></script>
     <![endif]-->
-    <link rel="shortcut icon" href="/template/images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/template/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/template/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/template/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="/template/images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="/images/ico/apple-touch-icon-57-precomposed.png">
 </head><!--/head-->
 
 <body>
@@ -58,7 +58,7 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="logo pull-left">
-                            <a href="/"><img src="/template/images/home/logo.png" alt="" /></a>
+                            <a href="/"><img src="/images/home/logo.png" alt="" /></a>
                         </div>
                     </div>
                     <div class="col-sm-8">
@@ -132,14 +132,14 @@
 
 
 
-<script src="/template/js/jquery.js"></script>
-<script src="/template/js/jquery.cycle2.min.js"></script>
-<script src="/template/js/jquery.cycle2.carousel.min.js"></script>
-<script src="/template/js/bootstrap.min.js"></script>
-<script src="/template/js/jquery.scrollUp.min.js"></script>
-<script src="/template/js/price-range.js"></script>
-<script src="/template/js/jquery.prettyPhoto.js"></script>
-<script src="/template/js/main.js"></script>
+<script src="/js/jquery.js"></script>
+<script src="/js/jquery.cycle2.min.js"></script>
+<script src="/js/jquery.cycle2.carousel.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/jquery.scrollUp.min.js"></script>
+<script src="/js/price-range.js"></script>
+<script src="/js/jquery.prettyPhoto.js"></script>
+<script src="/js/main.js"></script>
 
 
 </body>
